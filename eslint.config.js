46d208e8ccf3e40fcaff5536dd01e8e18eee8a35
17hz <0x149527@gmail.com>
2024-07-18 17:40:03 +0800

    deps: eslint-plugin-react

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,38 @@
+import { fixupPluginRules, fixupConfigRules } from '@eslint/compat'
 import eslint from '@eslint/js'
 import tseslint from 'typescript-eslint'
 import globals from 'globals'
+import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
 
 export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
+  ...fixupConfigRules(pluginReactConfig),
+  {
+    name: 'react/refresh',
+    plugins: {
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      'react-refresh/only-export-components': [
+        'warn',
+        {
+          allowConstantExport: true,
+        },
+      ],
+    },
+  },
+  {
+    name: 'react/hooks',
+    plugins: {
+      'react-hooks': fixupPluginRules(reactHooks),
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+    },
+  },
   {
     name: 'main',
     languageOptions: {
@@ -12,6 +40,10 @@ export default tseslint.config(
         ...globals.browser,
         ...globals.node,
       },
+    },
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-no-target-blank': 'off',
     },
   },
 )
